bd6b6fd52b02eb48563a31e2fff4fa5df73b9552
RusSatellite <russatellite23@gmail.com>
2022-03-22 19:10:57 +0800

    add max button

--- components/Converter/FromConv.tsx
+++ components/Converter/FromConv.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-
+import { useContext } from 'react'
+import { ToggleContext } from '../Layout/Layout'
 import InputWithIncDec from "../InputWithIncDec"
 
 const WalletTitle = styled.label`
@@ -11,13 +12,24 @@ const WalletTitle = styled.label`
     background-color: ${props => props.slot!=='bFOT' && '#22053D !important'}
 `
 
+const MaxButton = styled.span`
+    margin-bottom: 20px;
+    padding: 5px !important;
+    width: 100px !important;
+    min-width: unset !important;
+    cursor: pointer;
+`
+
 const FromConv = ({
     from,
     handleBurnMinus,
     burnAmount,
     onBurnChange,
     handleBurnPlus,
+    balance,
+    handleChange
 }) => {
+    const toggle = useContext(ToggleContext)
     return (
         <div className="gFotCurrencyt-selection">
             <WalletTitle slot={from} className="wallet-title">
@@ -29,6 +41,12 @@ const FromConv = ({
                 onBurnChange={onBurnChange}
                 handleBurnPlus={handleBurnPlus}
             />
+            <MaxButton 
+                onClick={() => handleChange(balance)}
+                className={`default-btn  ${!toggle && 'secondary-btn outlined'}`}
+            >
+                Max
+            </MaxButton>
         </div>
     )
 }
